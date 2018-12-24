b689dd65bbaf2dbd22041c74c174239562df7f25
Cristi Minica <cristi.minica@caphyon.com>
2018-12-24 16:47:46 +0200

    Some refactoring.

--- ex/ex7.js
+++ ex/ex7.js
@@ -1,20 +1,24 @@
-const Container = require('../lib/container');
-const Maybe = require('../lib/maybe');
+const Container = require('../lib/algebraic/container');
+const Maybe = require('../lib/Maybe');
 
+const maybe = require('../lib/essentials/maybe');
 const append = require('../lib/append');
+const curry = require('../lib/essentials/curry');
 const prop = require('../lib/prop');
 const match = require('../lib/match');
 const add = require('../lib/add');
+const compose = require('../lib/compose');
+const map = require('../lib/map');
 
 // (a -> b) -> Container a -> Container b
 Container.prototype.map = function (f) {
   return Container.of(f(this.$value));
 };
 
-Container.of(2).map(two => two + 2); 
+Container.of(2).map(two => two + 2);
 // Container(4)
 
-Container.of('flamethrowers').map(s => s.toUpperCase()); 
+Container.of('flamethrowers').map(s => s.toUpperCase());
 // Container('FLAMETHROWERS')
 
 console.log(Container.of('bombs').map(append(' away')).map(prop('length')));
@@ -31,10 +35,35 @@ Maybe.of({ name: 'Boris' }).map(prop('age')).map(add(10));
 
 console.log(Maybe.of({ name: 'Dinah', age: 14 }).map(prop('age')).map(add(10)));
 // Just(24)
+// safeHead :: [a] -> Maybe(a)
+const safeHead = xs => Maybe.of(xs[0]);
 
-/*
-Either.of('rain').map(str => `b${str}`); // Right('brain')
-left('rain').map(str => `It's gonna ${str}, better bring your umbrella!`); // Left('rain')
-Either.of({ host: 'localhost', port: 80}).map(prop('host')); // Right('localhost')
-left('rolls eyes...').map(prop('host')); // left ('rolls eyes..')
-*/
+// streetName :: Object -> Maybe String
+const streetName = compose(compose(map(prop('street')), safeHead), prop('addresses'));
+
+streetName({ addresses: [] });
+// Nothing
+
+console.log(streetName({ addresses: [{ street: 'Shady Ln.', number: 4201 }] }));
+// Just('Shady Ln.')
+
+const withdraw = curry((amount, { balance }) => Maybe.of(balance >= amount ? { balance: balance - amount } : null));
+const updateLedger = account => account;
+const remainingBalance = ({ balance }) => `Your balance is $${balance}`;
+
+const finishTransaction = compose(remainingBalance, updateLedger);
+console.log(finishTransaction({ balance: 20 }));
+var getTwenty = compose(map(finishTransaction), withdraw(20))
+
+console.log(getTwenty({ balance: 2000 }));
+console.log(getTwenty({ balance: 5 }));
+// https://mostly-adequate.gitbooks.io/mostly-adequate-guide/ch08.html#use-cases
+
+// getTwenty :: Account -> String
+getTwenty = compose(maybe('You\'re broke!', finishTransaction), withdraw(20));
+
+getTwenty({ balance: 200.00 }); 
+// 'Your balance is $180.00'
+
+getTwenty({ balance: 10.00 }); 
+// 'You\'re broke!'
