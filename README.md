18b579cbebb668b363ff62d7bc6009dae0a8d367
Cristi Minica <cristi.minica@caphyon.com>
2018-12-24 15:24:25 +0200

    Add new deps and examples. Add book link to README.

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 A repo with functional examples and library code.
 In the ex folder we mfind the examples files.
 In the lib folder we find the lib files.
+
+
+## Book link
+https://mostly-adequate.gitbooks.io/mostly-adequate-guide/
--- ex/ex7.js
+++ ex/ex7.js
@@ -1,7 +1,11 @@
+const Container = require('../lib/container');
+const append = require('../lib/append');
+const prop = require('../lib/prop');
 // (a -> b) -> Container a -> Container b
 Container.prototype.map = function (f) {
   return Container.of(f(this.$value));
 };
+
 Container.of(2).map(two => two + 2); 
 // Container(4)
 
@@ -10,3 +14,9 @@ Container.of('flamethrowers').map(s => s.toUpperCase());
 
 Container.of('bombs').map(append(' away')).map(prop('length')); 
 // Container(10)
+/*
+Either.of('rain').map(str => `b${str}`); // Right('brain')
+left('rain').map(str => `It's gonna ${str}, better bring your umbrella!`); // Left('rain')
+Either.of({ host: 'localhost', port: 80}).map(prop('host')); // Right('localhost')
+left('rolls eyes...').map(prop('host')); // left ('rolls eyes..')
+*/
