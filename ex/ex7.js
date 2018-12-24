bc41d5f6b526a4d5d35247e51b51eb67bac1800b
Cristi Minica <cristi.minica@caphyon.com>
2018-12-24 15:34:15 +0200

    Continue examples. Add needed functions. Minor bugfix.

--- ex/ex7.js
+++ ex/ex7.js
@@ -1,6 +1,11 @@
 const Container = require('../lib/container');
+const Maybe = require('../lib/maybe');
+
 const append = require('../lib/append');
 const prop = require('../lib/prop');
+const match = require('../lib/match');
+const add = require('../lib/add');
+
 // (a -> b) -> Container a -> Container b
 Container.prototype.map = function (f) {
   return Container.of(f(this.$value));
@@ -12,8 +17,21 @@ Container.of(2).map(two => two + 2);
 Container.of('flamethrowers').map(s => s.toUpperCase()); 
 // Container('FLAMETHROWERS')
 
-Container.of('bombs').map(append(' away')).map(prop('length')); 
+console.log(Container.of('bombs').map(append(' away')).map(prop('length')));
 // Container(10)
+
+Maybe.of('Malkovich Malkovich').map(match(/a/ig));
+// Just(True)
+
+Maybe.of(null).map(match(/a/ig));
+// Nothing
+
+Maybe.of({ name: 'Boris' }).map(prop('age')).map(add(10));
+// Nothing
+
+console.log(Maybe.of({ name: 'Dinah', age: 14 }).map(prop('age')).map(add(10)));
+// Just(24)
+
 /*
 Either.of('rain').map(str => `b${str}`); // Right('brain')
 left('rain').map(str => `It's gonna ${str}, better bring your umbrella!`); // Left('rain')
