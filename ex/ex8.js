c0dcadcd040a4a1473edb58245a4e4c747b0751b
Cristi Minica <cristi.minica@caphyon.com>
2018-12-27 16:29:55 +0200

    Reset progress.

--- ex/ex8.js
+++ ex/ex8.js
@@ -1,20 +1,29 @@
+const moment = require('moment');
+
+const curry = require('../lib/essentials/curry');
+const compose = require('../lib/essentials/compose');
+const map = require('../lib').POINTFREE.map;
+const Maybe = require('../lib').ALGEBRAIC.Maybe;
+const Either = require('../lib').ALGEBRAIC.Either;
+const maybe = require('../lib').ESSENTIALS.maybe;
+const f = require('../lib');
 // withdraw :: Number -> Account -> Maybe(Account)
-const withdraw = curry((amount, { balance }) =>
+var withdraw = curry((amount, { balance }) =>
   Maybe.of(balance >= amount ? { balance: balance - amount } : null));
 
 // This function is hypothetical, not implemented here... nor anywhere else.
 // updateLedger :: Account -> Account 
-const updateLedger = account => account;
+var updateLedger = account => account;
 
 // remainingBalance :: Account -> String
-const remainingBalance = ({ balance }) => `Your balance is $${balance}`;
+var remainingBalance = ({ balance }) => `Your balance is $${balance}`;
 
 // finishTransaction :: Account -> String
-const finishTransaction = compose(remainingBalance, updateLedger);
+var finishTransaction = compose(remainingBalance, updateLedger);
 
 
 // getTwenty :: Account -> Maybe(String)
-const getTwenty = compose(map(finishTransaction), withdraw(20));
+var getTwenty = compose(map(finishTransaction), withdraw(20));
 
 getTwenty({ balance: 200.00 }); 
 // Just('Your balance is $180')
@@ -22,17 +31,8 @@ getTwenty({ balance: 200.00 });
 getTwenty({ balance: 10.00 });
 // Nothing
 
-// maybe :: b -> (a -> b) -> Maybe a -> b
-const maybe = curry((v, f, m) => {
-  if (m.isNothing) {
-    return v;
-  }
-
-  return f(m.$value);
-});
-
 // getTwenty :: Account -> String
-const getTwenty = compose(maybe('You\'re broke!', finishTransaction), withdraw(20));
+var getTwenty = compose(maybe('You\'re broke!', finishTransaction), withdraw(20));
 
 getTwenty({ balance: 200.00 }); 
 // 'Your balance is $180.00'
@@ -42,12 +42,12 @@ getTwenty({ balance: 10.00 });
 
 
 // getAge :: Date -> User -> Either(String, Number)
-const getAge = curry((now, user) => {
-  const birthDate = moment(user.birthDate, 'YYYY-MM-DD');
+var getAge = curry((now, user) => {
+  var birthDate = moment(user.birthDate, 'YYYY-MM-DD');
 
   return birthDate.isValid()
     ? Either.of(now.diff(birthDate, 'years'))
-    : left('Birth date could not be parsed');
+    : f.left('Birth date could not be parsed');
 });
 
 getAge(moment(), { birthDate: '2005-12-12' });
@@ -55,136 +55,3 @@ getAge(moment(), { birthDate: '2005-12-12' });
 
 getAge(moment(), { birthDate: 'July 4, 2001' });
 // Left('Birth date could not be parsed')
-
-// zoltar :: User -> _
-const zoltar = compose(console.log, either(id, fortune), getAge(moment()));
-
-zoltar({ birthDate: '2005-12-12' });
-// 'If you survive, you will be 10'
-// undefined
-
-zoltar({ birthDate: 'balloons!' });
-// 'Birth date could not be parsed'
-// undefined
-
-// fortune :: Number -> String
-const fortune = compose(append('If you survive, you will be '), toString, add(1));
-
-// zoltar :: User -> Either(String, _)
-const zoltar = compose(map(console.log), map(fortune), getAge(moment()));
-
-zoltar({ birthDate: '2005-12-12' });
-// 'If you survive, you will be 10'
-// Right(undefined)
-
-zoltar({ birthDate: 'balloons!' });
-// Left('Birth date could not be parsed')
-
-// ioWindow :: IO Window
-const ioWindow = new IO(() => window);
-
-ioWindow.map(win => win.innerWidth);
-// IO(1430)
-
-ioWindow
-  .map(prop('location'))
-  .map(prop('href'))
-  .map(split('/'));
-// IO(['http:', '', 'localhost:8000', 'blog', 'posts'])
-
-
-// $ :: String -> IO [DOM]
-const $ = selector => new IO(() => document.querySelectorAll(selector));
-
-$('#myDiv').map(head).map(div => div.innerHTML);
-// IO('I am some inner html')
-// url :: IO String
-const url = new IO(() => window.location.href);
-
-// toPairs :: String -> [[String]]
-const toPairs = compose(map(split('=')), split('&'));
-
-// params :: String -> [[String]]
-const params = compose(toPairs, last, split('?'));
-
-// findParam :: String -> IO Maybe [String]
-const findParam = key => map(compose(Maybe.of, filter(compose(eq(key), head)), params), url);
-
-// -- Impure calling code ----------------------------------------------
-
-// run it by calling $value()!
-findParam('searchTerm').$value();
-// Just([['searchTerm', 'wafflehouse']])
-
-// -- Node readFile example ------------------------------------------
-
-const fs = require('fs');
-
-// readFile :: String -> Task Error String
-const readFile = filename => new Task((reject, result) => {
-  fs.readFile(filename, (err, data) => (err ? reject(err) : result(data)));
-});
-
-readFile('metamorphosis').map(split('\n')).map(head);
-// Task('One morning, as Gregor Samsa was waking up from anxious dreams, he discovered that
-// in bed he had been changed into a monstrous verminous bug.')
-
-
-// -- jQuery getJSON example -----------------------------------------
-
-// getJSON :: String -> {} -> Task Error JSON
-const getJSON = curry((url, params) => new Task((reject, result) => {
-  $.getJSON(url, params, result).fail(reject);
-}));
-
-getJSON('/video', { id: 10 }).map(prop('title'));
-// Task('Family Matters ep 15')
-
-
-// -- Default Minimal Context ----------------------------------------
-
-// We can put normal, non futuristic values inside as well
-Task.of(3).map(three => three + 1);
-// Task(4)
-// -- Pure application -------------------------------------------------
-// blogPage :: Posts -> HTML
-const blogPage = Handlebars.compile(blogTemplate);
-
-// renderPage :: Posts -> HTML
-const renderPage = compose(blogPage, sortBy(prop('date')));
-
-// blog :: Params -> Task Error HTML
-const blog = compose(map(renderPage), getJSON('/posts'));
-
-
-// -- Impure calling code ----------------------------------------------
-blog({}).fork(
-  error => $('#error').html(error.message),
-  page => $('#main').html(page),
-);
-
-$('#spinner').show();
-
-// topRoute :: String -> Maybe String
-const topRoute = compose(Maybe.of, reverse);
-
-// bottomRoute :: String -> Maybe String
-const bottomRoute = compose(map(reverse), Maybe.of);
-
-topRoute('hi'); // Just('ih')
-bottomRoute('hi'); // Just('ih')
-
-const nested = Task.of([Either.of('pillows'), left('no sleep for you')]);
-
-map(map(map(toUpperCase)), nested);
-// Task([Right('PILLOWS'), Left('no sleep for you')])
-
-const tmd = Task.of(Maybe.of('Rock over London'));
-
-const ctmd = Compose.of(tmd);
-
-const ctmd2 = map(append(', rock on, Chicago'), ctmd);
-// Compose(Task(Just('Rock over London, rock on, Chicago')))
-
-ctmd2.getCompose;
-// Task(Just('Rock over London, rock on, Chicago'))
