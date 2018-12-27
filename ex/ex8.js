c27cc5fee3288e2ffe0182da0c156a60382cc2cf
Cristi Minica <cristi.minica@caphyon.com>
2018-12-27 18:15:05 +0200

    Add progress.

--- ex/ex8.js
+++ ex/ex8.js
@@ -25,7 +25,7 @@ var finishTransaction = compose(remainingBalance, updateLedger);
 // getTwenty :: Account -> Maybe(String)
 var getTwenty = compose(map(finishTransaction), withdraw(20));
 
-getTwenty({ balance: 200.00 }); 
+getTwenty({ balance: 200.00 });
 // Just('Your balance is $180')
 
 getTwenty({ balance: 10.00 });
@@ -34,10 +34,10 @@ getTwenty({ balance: 10.00 });
 // getTwenty :: Account -> String
 var getTwenty = compose(maybe('You\'re broke!', finishTransaction), withdraw(20));
 
-getTwenty({ balance: 200.00 }); 
+getTwenty({ balance: 200.00 });
 // 'Your balance is $180.00'
 
-getTwenty({ balance: 10.00 }); 
+getTwenty({ balance: 10.00 });
 // 'You\'re broke!'
 
 
@@ -53,5 +53,22 @@ var getAge = curry((now, user) => {
 getAge(moment(), { birthDate: '2005-12-12' });
 // Right(9)
 
-getAge(moment(), { birthDate: 'July 4, 2001' });
+console.log(getAge(moment(), { birthDate: 'July 4, 2001' }));
 // Left('Birth date could not be parsed')
+var fortune = compose(f.toString, f.add(1));
+console.log(fortune(2))
+fortune = compose(f.concat('If you survive you will be '), fortune)
+
+var zoltar = compose(map(fortune), getAge(moment()));
+zoltar = compose(map(console.log), zoltar);
+zoltar({ birthDate: '2005-12-12' });
+// 'If you survive, you will be 10'
+// Right(undefined)
+
+zoltar({ birthDate: 'balloons!' });
+// Left('Birth date could not be parsed')
+// -- Default Minimal Context ----------------------------------------
+
+// We can put normal, non futuristic values inside as well
+f.Task.of(3).map(three => three + 1);
+// Task(4)
